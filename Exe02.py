e21d3545c83b08165f172882ef8817e2905c4321
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-07 19:21:57 -0300

    update

---
+++ Exe02.py
@@ -0,0 +1,5 @@
+# Escreva um programa que exiba o resultado de 2a X 3b, em que 'a' vale 3 e 'b' vale 5
+a = 3
+b = 5
+r = 2*a*3*b
+print (r)
---
+++ Exe03.py
@@ -0,0 +1,6 @@
+#Escreva um programa que calcule a soma de tres variaveis e imprima o resultado na tela
+n1 = float (input('Digite o primeiro numero: ' ))
+n2 = float (input('Digite o segundo numero: ' ))
+n3 = float (input('Digite o terceiro numero: ' ))
+resultado =  n1 + n2 + n3
+print(resultado)
