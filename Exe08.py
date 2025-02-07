fa424afa09080859b661ed0575df45e2187ee1a7
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-07 19:58:08 -0300

    update

---
+++ Exe08.py
@@ -0,0 +1,5 @@
+#Escreva um programa que peça ao usuario para inserir dois numeros inteiro, some-os e exiba a mensagem: "O total é: [soma dos dois valores]"
+n1 = float (input('Digite o primeiro número: '))
+n2 = float (input('Digite o segundo número: '))
+resultado = n1 + n2
+print (resultado)
